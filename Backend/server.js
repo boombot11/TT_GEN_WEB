4198dc6df8c516e4eda6e210222ebac519568662
boombot11 <sahilsanskruti11@gmail.com>
2025-02-01 13:46:41 +0530

    platform check

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,25 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/authRoutes.js";
-// import authRoutes from "./routes/authRoutes.js";
+
+// Ensure it’s running on Windows before starting the server
+if (process.platform !== 'win32') {
+    console.log('This application is only supported on Windows.');
+    process.exit(1); // Exit the app if not on Windows
+}
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(cors({ origin: '*' }));
-// Routes
-// app.use("/auth", authRoutes);
+
 // Routes
 const PORT = process.env.PORT || 5000; // Use the environment variable for PORT or fallback to 5000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
 app.use((req, res, next) => {
     res.setTimeout(10 * 60 * 1000, () => { // 10 minutes timeout
         console.log('Request timed out.');
@@ -22,5 +27,7 @@ app.use((req, res, next) => {
     });
     next();
 });
+
 app.use(router);
+
 export default app;
